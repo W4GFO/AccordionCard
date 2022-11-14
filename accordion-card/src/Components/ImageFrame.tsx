93a0ecdd0107b05f03c43e703bf673942f83d34c
Jeff Behrbaum <sejapresident@gmail.com>
2022-11-14 15:42:39 -0500

    Added in the secondary box image.

--- accordion-card/src/Components/ImageFrame.tsx
+++ accordion-card/src/Components/ImageFrame.tsx
@@ -1,12 +1,22 @@
 import './ImageFrameStyle.scss'
-import woman from '../Images/illustration-woman-online-mobile.svg'
+import woman_mobile from '../Images/illustration-woman-online-mobile.svg'
+import woman_desktop from '../Images/illustration-woman-online-desktop.svg'
+import desktop_box from '../Images/illustration-box-desktop.svg'
 
 type ImageFrameProps = {}
 
 export const ImageFrame = (props: ImageFrameProps) => {
 	return (
 		<section className='image-outer-frame'>
-			<img src={woman} alt="foo"/>
+			<div className='images-container'>
+				<picture>
+					<source media='(min-width:1080px)' srcSet={woman_desktop} />
+					<img src={woman_mobile} alt="working woman"/>
+				</picture>
+
+				<img className='desktop-box-frame' src={desktop_box} alt='box on floor'/>
+
+			</div>
 		</section>
 	)
 }
